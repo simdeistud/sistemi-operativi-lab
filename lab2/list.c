c7e76d5e98f827b90251fefcad16bd64d5c156f8
simdeistud <simone.deiana@studenti.units.it>
2024-04-29 10:36:52 +0200

    completed exercise lab2

--- lab2/list.c
+++ lab2/list.c
@@ -1,183 +1,150 @@
+#include <sys/types.h>
 #include <dirent.h>
-#include <stdlib.h>
-#include <stdint.h>
 #include <stdio.h>
 #include <sys/stat.h>
+#include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <grp.h>
 #include <pwd.h>
-#include <sys/types.h>
+#include <grp.h>
 
-typedef struct {
-    char relative_path[512];
-    ino_t inode_number;
-    mode_t type;
-    char type_name[32];
-    off_t size;
-    uid_t owner;
-    char owner_name[128];
-    gid_t group;
-    char group_name[128];
-} entry_info;
+#define MAX_DEPTH 4096
+#define MAX_PATH_LENGTH 1024
 
-void print_entry_info(entry_info* info);
+int printFileInfo(char path[]);
 
-entry_info get_entry_info(char entry_path[], char relative_base_path[], char entry_name[]);
+int main(int argc, char* argv[])
+{
 
-void put_type_name(char type_name[], unsigned short type);
+	// checks if number of arguments is correct
+	if (argc != 2) {
+		printf("Invalid number of arguments.\n");
+		return 1;
+	}
 
-void put_owner_name(char owner_name[], short uid);
+	// tries to open the folder specified in the second argument
+	DIR* starting_dir;
+	if ( (starting_dir = opendir(argv[1]) ) == NULL) {
+		perror("Error opening starting folder");
+		return 1;
+	}
+	if (closedir(starting_dir) == -1) {
+		perror("Error closing starting folder");
+		return 1;
+	}
 
-void put_group_name(char group_name[], short gid);
+	// prints information of the starting folder
+	printFileInfo(argv[1]);
 
-void list(DIR *dir, char relative_path[]);
+	// creates array containing the paths of all the directories to explore
+	char dirs_paths[MAX_DEPTH][MAX_PATH_LENGTH];
+	for (int c = 0; c < MAX_DEPTH; c++) {
+		strcpy(dirs_paths[c], "");
+	}
+	strcpy(dirs_paths[0], argv[1]);
+	
 
-int main(int argc, char *argv[]) {
+	// explores the folders
+	DIR* current_dir_entry;
+	for (int c = 0, last_dir = 0; (current_dir_entry = opendir(dirs_paths[c])) != NULL || c <= last_dir; c++) {
+		struct dirent *current_entry;
 
-    // controllare se gli argomenti sono giusti
-    if (argc != 2) {
-        printf("Wrong number of arguments!\n");
-        return 1;
-    }
+		while ( (current_entry = readdir(current_dir_entry)) != NULL ) {
 
-    // controllare se argv[1] riferisce una cartella
+			// we don't consider the "." and ".." entries
+			if ( strcmp(current_entry->d_name, ".") == 0 || strcmp(current_entry->d_name, "..") == 0 ) {
+				continue;
+			}
 
-    struct stat dir_stat;
-    int has_failed = stat(argv[1], &dir_stat);
-    if(has_failed == -1){
-                perror("stat fallita");
-                exit(EXIT_FAILURE);
-            }
+			// build the full path of the file
+			char full_path[MAX_PATH_LENGTH];
+			strcpy(full_path, dirs_paths[c]);
+			strcat(full_path, "/");
+			strcat(full_path, current_entry->d_name);
 
-    if (!S_ISDIR(dir_stat.st_mode)) {
-        printf("Path does not refer to a folder!\n");
-        return 1;
-    }
+			// print the info of the file
+			if (printFileInfo(full_path)) {
+				printf("Unable to retrieve info about file %s", full_path);
+				perror("");
+				return 1;
+			}
 
-    // stampa le info della cartella iniziale
+			// if it's a folder, add it to the dirs_paths array
+			struct stat dir_stat;
+			if (stat(full_path, &dir_stat) == -1) {
+				perror("stat failed");
+				return 1;
+			}
+			if (S_ISDIR(dir_stat.st_mode) && ++last_dir < MAX_DEPTH) {
+				strcpy(dirs_paths[last_dir], full_path);
+			}
+		
+		}
 
-    entry_info root_info = get_entry_info(argv[1], "", argv[1]);
-    print_entry_info(&root_info);
-
-    // apre la cartella iniziale e chiama list()
-
-    DIR *starting_dir = opendir(argv[1]);
-
-    char relative_path[256];
-    sprintf(relative_path, "%s/", argv[1]);
-    list(starting_dir, relative_path);
-    closedir(starting_dir);
-
-    return 0;
+		if (closedir(current_dir_entry) == -1) {
+			printf("Error closing folder %s", dirs_paths[c]);
+			perror("");
+			return 1;
+		}
+	}
 
 }
 
-void list(DIR *dir, char relative_path[]) {
+int printFileInfo(char path[])
+{
+	// using stat() to find the information of the file
+	struct stat file_stat;
+	if (stat(path, &file_stat) == -1) {
+		return 1;
+	}
 
-    DIR *subdirs[256];
-    char *subdirs_name[256];
-    int subdirs_num = 0;
+	// discerning the type of the file
+	char type[32];
+	switch (file_stat.st_mode & S_IFMT) {
+		case S_IFREG:
+			strcpy(type, "file");
+			break;
+		case S_IFDIR:	
+			strcpy(type, "directory");
+			break;
+		case S_IFIFO:
+			strcpy(type, "FIFO");
+			break;
+		case S_IFLNK:
+			strcpy(type, "symbolic link");
+			break;
+		default:
+			strcpy(type, "other");
+	}
 
-    struct dirent *dir_entry;
-    while ((dir_entry = readdir(dir)) != NULL) {
-        if (strcmp(dir_entry->d_name, ".") != 0 && strcmp(dir_entry->d_name, "..") != 0) {
-            char entry_path[512];
-            strcpy(entry_path, relative_path);
-            strcat(entry_path, dir_entry->d_name);
-            entry_info info = get_entry_info(entry_path, relative_path, dir_entry->d_name);
-            print_entry_info(&info);
+	// obtaining the name of the user
+	struct passwd* file_usr = getpwuid(file_stat.st_uid);
+	if (file_usr == NULL) {
+		return 1;
+	}
+	
+	// obtaining the name of the group
+	struct group* file_grp = getgrgid(file_stat.st_gid);
+	if (file_grp == NULL) {
+		return 1;
+	}
 
-            struct stat entry_stat;
-            int has_failed = stat(entry_path, &entry_stat);
-            if(has_failed == -1){
-                perror("stat fallita");
-                
-                exit(EXIT_FAILURE);
-            }
-            if (S_ISDIR(entry_stat.st_mode)) {
-                subdirs[subdirs_num] = opendir(entry_path);
-                subdirs_name[subdirs_num] = dir_entry->d_name;
-                subdirs_num++;
-            }
-        }
+	printf("Node: %s\n", path);
+	printf("    ");
 
-    }
+	printf("Inode: %u\n", file_stat.st_ino);
+	printf("    ");
 
-    while (--subdirs_num >= 0) {
-        char new_relative_path[256];
-        strcpy(new_relative_path, relative_path);
-        strcat(new_relative_path, subdirs_name[subdirs_num]);
-        strcat(new_relative_path, "/");
-        list(subdirs[subdirs_num], new_relative_path);
-        closedir(subdirs[subdirs_num]);
-    }
+	printf("Type: %s\n", type);
+	printf("    ");
+
+	printf("Size: %u\n", file_stat.st_size);
+	printf("    ");
+
+	printf("Owner: %u %s\n", file_stat.st_uid, file_usr->pw_name);
+	printf("    ");
+
+	printf("Group: %u %s\n", file_stat.st_gid, file_grp->gr_name);
+	printf("\n");
+
+	return 0;
 }
-
-void print_entry_info(entry_info* info) {
-    printf("Node: %s\n", info->relative_path);
-    printf("    ");
-    printf("Inode: %ju\n", (uintmax_t) info->inode_number);
-    printf("    ");
-    printf("Type: %s\n", info->type_name);
-    printf("    ");
-    printf("Size: %jd\n", (uintmax_t) info->size);
-    printf("    ");
-    printf("Owner: %jd %s\n", (uintmax_t) info->owner, info->owner_name);
-    printf("    ");
-    printf("Group: %jd %s\n", (uintmax_t) info->group, info->group_name);
-}
-
-entry_info get_entry_info(char entry_path[], char relative_base_path[], char entry_name[]) {
-
-    struct stat entry_stat; 
-    int has_failed = stat(entry_path, &entry_stat); // la stat non viene aperta correttamente
-    if(has_failed == -1){
-                perror("stat fallita");
-                exit(EXIT_FAILURE);
-            }
-
-    entry_info info;
-    strcpy(info.relative_path, entry_path);
-    info.inode_number = entry_stat.st_ino;
-    info.type = entry_stat.st_mode;
-    put_type_name(info.type_name, info.type);
-    info.size = entry_stat.st_size;
-    info.owner = entry_stat.st_uid;
-    put_owner_name(info.owner_name, info.owner);
-    info.group = entry_stat.st_gid;
-    put_group_name(info.group_name, info.group);
-
-    return info;
-}
-
-void put_owner_name(char owner_name[], short uid) {
-    struct passwd *entry_passwd = getpwuid(uid);
-    strcpy(owner_name, entry_passwd->pw_name);
-}
-
-void put_group_name(char group_name[], short gid) {
-    struct group *entry_group = getgrgid(gid);
-    strcpy(group_name, entry_group->gr_name);
-}
-
-void put_type_name(char type_name[], unsigned short type) {
-    switch (type & S_IFMT) {
-        case S_IFDIR:
-            sprintf(type_name, "directory");
-            break;
-        case S_IFIFO:
-            sprintf(type_name, "FIFO");
-            break;
-        case S_IFLNK:
-            sprintf(type_name, "symbolic link");
-            break;
-        case S_IFREG:
-            sprintf(type_name, "file");
-            break;
-        default:
-            sprintf(type_name, "other");
-            break;
-    }
-}
-
