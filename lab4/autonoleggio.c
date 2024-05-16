d56511b33345fd29a6a6445e4795fddab36b4fe6
simdeistud <simone.deiana@studenti.units.it>
2024-05-16 17:03:37 +0200

    Completed exercise

--- lab4/autonoleggio.c
+++ lab4/autonoleggio.c
@@ -6,12 +6,12 @@
 #include <sys/stat.h>
 #include <semaphore.h>
 
-#define INPUT_MAX 20
-#define CAR_NAME_MAX 10
+#define INPUT_MAX 30
+#define CAR_NAME_MAX 20
 #define SEMAPHORE_PREFIX "/autonoleggio_"
 #define SEMAPHORE_PREFIX_LEN  14
 
-sem_t * sem_reading;
+sem_t * sem_starting, * sem_reading;
 
 void create_semaphores();
 void view_cars_status();
@@ -25,8 +25,9 @@ int main(int argc, char* argv[])
     }
 
     /* controllo se il programma è stato già avviato in precedenza, 
-       in caso contrario inizializzo i semafori per ogni vettura    */
-    sem_t * sem_starting = sem_open("/autonoleggio_starting", O_EXCL, S_IRUSR | S_IWUSR);
+       in caso contrario inizializzo i semafori per ogni vettura
+    */
+    sem_starting = sem_open("/autonoleggio_starting", O_EXCL, S_IRUSR | S_IWUSR);
     if(sem_starting == SEM_FAILED){
         sem_starting = sem_open("/autonoleggio_starting", O_CREAT, S_IRUSR | S_IWUSR, 0);
         create_semaphores();
@@ -34,8 +35,8 @@ int main(int argc, char* argv[])
     }
     /* se il semaforo di inizializzazione esiste mi assicuro che
        l'inizializzazione sia terminata nel caso il processo corrente
-       non sia quello che l'ha fatta partire 
-                                */
+       non sia quello che l'ha fatta partire
+    */
     sem_wait(sem_starting);
     sem_post(sem_starting);
     sem_reading = sem_open("/autonoleggio_reading", O_CREAT, S_IRUSR | S_IWUSR, 0);
@@ -47,23 +48,26 @@ int main(int argc, char* argv[])
         input[strcspn(input, "\n")] = '\0';
         char * command = strtok(input, " ");
         char * argument = strtok(NULL, " ");
-        if(strncmp(command, "quit", 4) == 0 && argument == NULL){
-            break;
-        } else if(strncmp(command, "view", 4) == 0 && argument == NULL){
-            view_cars_status();
-        } else if(strncmp(command, "lock", 4) == 0 ){
-            if(strlen(argument) > CAR_NAME_MAX){
-                printf("Error: car name too long\n");
+        if(argument == NULL){
+            if(strncmp(command, "quit", 4) == 0){
+                break;
+            }
+            if(strncmp(command, "view", 4) == 0){
+                view_cars_status();
                 continue;
             }
-            lock_car(argument);
-        } else if(strncmp(command, "release", 7) == 0){
-            if(strlen(argument) > CAR_NAME_MAX){
-                printf("Error: car name too long\n");
+        }
+        if(argument != NULL){
+            if(strncmp(command, "lock", 4) == 0 && strlen(argument) < CAR_NAME_MAX){
+                lock_car(argument);
                 continue;
             }
-            release_car(argument);
-        } else printf("Unknown Command\n");
+            if(strncmp(command, "release", 7) == 0 && strlen(argument) < CAR_NAME_MAX){
+                release_car(argument);
+                continue;
+            }
+        }
+        printf("Unknown Command\n");
     }
 
     exit(0);
@@ -80,7 +84,8 @@ void create_semaphores()
     }
     char car_name[CAR_NAME_MAX];
     char semaphore_name[CAR_NAME_MAX + SEMAPHORE_PREFIX_LEN + 1];
-    while(fscanf(catalog, "%s", car_name) != EOF){
+    while(fgets(car_name, CAR_NAME_MAX, catalog) != NULL){
+        car_name[strcspn(car_name, "\n")] = '\0';
         strcpy(semaphore_name, SEMAPHORE_PREFIX);
         strcat(semaphore_name, car_name);
         if(sem_open(semaphore_name, O_CREAT | O_EXCL, S_IRUSR | S_IWUSR, 1) == SEM_FAILED){
@@ -113,9 +118,9 @@ void view_cars_status()
         unsigned int status;
         sem_getvalue(sem_open(semaphore_name, O_CREAT, S_IRUSR | S_IWUSR, 0), &status);
         printf("Car: ");
-        printf(car_name);
+        printf("%s", car_name);
         printf(", status: ");
-        printf(status == 1 ? "free" : "locked");
+        printf(status == 1 ? "free" : "busy");
         printf("\n");
     }
     fclose(catalog);
@@ -129,7 +134,7 @@ void lock_car(char car_name[])
     strcat(semaphore_name, car_name);
     if(sem_open(semaphore_name, O_EXCL, S_IRUSR | S_IWUSR) == SEM_FAILED){
         printf("Cannot find car ");
-        printf(car_name);
+        printf("%s", car_name);
         printf("\n");
     } else{
         sem_t * car_to_lock = sem_open(semaphore_name, O_CREAT, S_IRUSR | S_IWUSR, 0);
@@ -137,13 +142,13 @@ void lock_car(char car_name[])
         sem_getvalue(sem_open(semaphore_name, O_CREAT, S_IRUSR | S_IWUSR, 0), &status);
         if(status == 0){
             printf("Error. Car: ");
-            printf(car_name);
+            printf("%s", car_name);
             printf(" already locked\n");
         }
         if(status == 1){
             sem_wait(car_to_lock);
             printf("Car: ");
-            printf(car_name);
+            printf("%s", car_name);
             printf(" is now locked\n");
         }
     }
@@ -156,7 +161,7 @@ void release_car(char car_name[])
     strcat(semaphore_name, car_name);
     if(sem_open(semaphore_name, O_EXCL, S_IRUSR | S_IWUSR) == SEM_FAILED){
         printf("Cannot find car ");
-        printf(car_name);
+        printf("%s", car_name);
         printf("\n");
     } else{
         sem_t * car_to_release = sem_open(semaphore_name, O_CREAT, S_IRUSR | S_IWUSR, 0);
@@ -165,12 +170,12 @@ void release_car(char car_name[])
         if(status == 0){
             sem_post(car_to_release);
             printf("Car: ");
-            printf(car_name);
+            printf("%s", car_name);
             printf(" is now free\n");
         }
         if(status == 1){
             printf("Error. Car: ");
-            printf(car_name);
+            printf("%s", car_name);
             printf(" already free\n");
         }
     }
